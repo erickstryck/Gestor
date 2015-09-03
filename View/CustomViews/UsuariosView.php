035160b41b3605a65fbfd94ea3f72bc9fdccac9e
Guepardo <bsinet@hotmail.com>
2015-09-03 11:09:57 -0300

    Adicionando moeda por extenso

--- View/CustomViews/UsuariosView.php
+++ View/CustomViews/UsuariosView.php
@@ -27,13 +27,13 @@ class UsuariosView extends GenericView{
 			$usuario->where('id = '. $associativa->usuarioId)->find();
 
 			while( $usuario->fetch()){
-				parent::$templator->setVariable('usuario.id', $usuario->id);
+				parent::$templator->setVariable('usuario.id', Convert::zeroEsquerda($usuario->id));
 				parent::$templator->setVariable('usuario.nome_completo', $usuario->nomeCompleto); 
 				parent::$templator->setVariable('usuario.email', $usuario->email); 
 				parent::$templator->setVariable('usuario.login', $usuario->login); 
-				parent::$templator->setVariable('usuario.acesso', ( $associativa->temAcesso ) ? "Sim" : "<b style='color:red'>Não</b>");
-				parent::$templator->setVariable('usuario.vendedor', ( $associativa->isVendedor ) ? "Sim" : "<b style='color:red'>Não</b>");
-				parent::$templator->setVariable('usuario.tecnico', ( $associativa->isTecnico ) ? "Sim" : "<b style='color:red'>Não</b>");
+				parent::$templator->setVariable('usuario.acesso', ( $associativa->temAcesso ) ? "Sim" : "<i style='color:red'>Não</i>");
+				parent::$templator->setVariable('usuario.vendedor', ( $associativa->isVendedor ) ? "Sim" : "<i style='color:red'>Não</i>");
+				parent::$templator->setVariable('usuario.tecnico', ( $associativa->isTecnico ) ? "Sim" : "<i style='color:red'>Não</i>");
 				parent::$templator->setVariable('usuario.permissoes', ( $associativa->isAdmin ) ? "Adminstrador" : "Uso restrito");
 
 				parent::$templator->addBlock('row'); 
