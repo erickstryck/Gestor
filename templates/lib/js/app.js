586bb908523df6e3a56fd1fe4b78b25c5ff1b3e5
Guepardo <bsinet@hotmail.com>
2015-09-09 08:43:35 -0300

    Alterando a função de deleção como um plugin

--- templates/lib/js/app.js
+++ templates/lib/js/app.js
@@ -1,15 +1,41 @@
 
 
 //Essa função tem dependência do método sendAjax() e Alertify; 
-function deletar(useCase, id){
-	var url = 'index.php?uc='+useCase+'&a=deletar'; 
+// function deletar(useCase, id){
+// 	var url = 'index.php?uc='+useCase+'&a=deletar'; 
 
-	sendAjax(url,'POST',{'id' : id},function(data){
-		data = JSON.parse(data);
+// 	sendAjax(url,'POST',{'id' : id},function(data){
+// 		data = JSON.parse(data);
 
-		if(data['status'])
-			alertify.success("Registro deletado com sucesso."); 
-		else
-			alertify.error(data['msg']); 
-	}); 
-}
+// 		if(data['status'])
+// 			alertify.success("Registro deletado com sucesso."); 
+// 		else
+// 			alertify.error(data['msg']); 
+// 	}); 
+// }
+
+(function($){
+	var interval; 
+
+	$.fn.deletar = function(useCase, delay){
+
+		if( typeof delay == 'undefined')
+			delay = 1500; 
+
+		this.click(function(){
+			var id  = this.value; 
+			var url = 'index.php?uc='+useCase+'&a=deletar'; 
+
+			sendAjax(url,'POST',{'id' : id},function(data){
+				data = JSON.parse(data);
+
+				if(data['status']){
+					alertify.success("Registro deletado com sucesso."); 
+					interval = setInterval(function(){window.location.reload(); clearInterval(interval); },delay); 
+				}
+				else
+					alertify.error(data['msg']); 
+			});
+		}); 
+	}
+}(jQuery))
