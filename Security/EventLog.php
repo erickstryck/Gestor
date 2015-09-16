8cffe945734787109d0cba235b585276e1878bf5
desenvolvedor <erickstryck@hotmail.com>
2015-09-16 13:01:55 -0300

    done

--- Security/EventLog.php
+++ Security/EventLog.php
@@ -12,7 +12,7 @@ class EventLog
         Lumine::import("Log");
         $data=strftime('%A, %d de %B de %Y', strtotime('today')).' as '.Date('H:i:s');
         $log = new Log();
-        $requisicao= self::getArray();
+        $requisicao= $_REQUEST;
         $log->ocorrencia=$data.' '.$msg;
         $temp='';
         foreach($_SESSION['Permissao'] as $valor){
@@ -24,15 +24,5 @@ class EventLog
         $log->usuarioId=$_SESSION["id"];
         $log->empresaId=$_SESSION["empresaId"];
         $log->insert();
-    }
-
-    private function getArray(){
-        if(strcmp($_SERVER['REQUEST_METHOD'],'POST')==0){
-            $post=$_POST;
-            return $post;
-        }else{
-            $get=$_GET;
-            return $get;
-        }
     }
 }
