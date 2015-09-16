f670825c17ed54a6829939a6ecf40c6c129840ec
Guepardo <bsinet@hotmail.com>
2015-09-16 11:01:29 -0300

    Adicionando o blog

--- Controller/UseCase/Autenticar.php
+++ Controller/UseCase/Autenticar.php
@@ -1,35 +1,20 @@
 <?php
+require_once(PATH . 'Controller' . DS . 'GenericController.php');
+require_once(PATH . 'View' . DS . 'CustomViews' . DS . 'AutenticarView.php');
 
-
-class Autenticar extends GenericController
-{
+class Autenticar extends GenericController{
     private $autenticarView;
 
-    public function __construct()
-    {
+    public function __construct(){
         $this->autenticarView = new AutenticarView();
     }
 
-    public function loginView()
-    {
+    public function loginView(){
         $this->autenticarView->loginView();
     }
 
-    public function login($arg)
-    {
-        $daoUsuario   = new DAOUsuario();
-        $daoEgresso   = new DAOEgresso();
-        $daoProfessor = new DAOProfessor();
-
-        $usuario = $daoUsuario->selectByCpfPassword($arg['cpf'], $arg['password']);
-        $status = !is_string($usuario);
-
-        if ($status) {
-            $_SESSION['id_user'] = (int)$usuario->getId();
-            $_SESSION['egresso'] = ($daoEgresso->isEgresso($_SESSION['id_user'])) ? true : false;
-        }
-
-        $this->autenticarView->sendAjax(array("status" => $status));
+    public function login($arg){
+        
     }
 
 }
