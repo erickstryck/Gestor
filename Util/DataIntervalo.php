9fbfb8f8657c808be9d6bc2594e51446490a97cb
Guepardo <bsinet@hotmail.com>
2015-10-02 13:06:03 -0300

    Correção na classe de formatação de datas.

--- Util/DataIntervalo.php
+++ Util/DataIntervalo.php
@@ -7,40 +7,35 @@ class DataIntervalo{
 
 
 	public static function dias($date){
-		return date('d/m/Y', strtotime('+1 day', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+1 day', strtotime($date)));
 	}
 
 	public static function semanas($date){
-		return date('d/m/Y', strtotime('+1 week', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+1 week', strtotime($date)));
 	}
 
 	public static function quinzenas($date){
-		return date('d/m/Y', strtotime('+15 days', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+15 days', strtotime($date)));
 	}
 
 	public static function meses($date){
-		return date('d/m/Y', strtotime('+1 month', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+1 month', strtotime($date)));
 	}
 
 	public static function bimestres($date){
-		return date('d/m/Y', strtotime('+2 months', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+2 months', strtotime($date)));
 	}
 
 	public static function trimestres($date){
-		return date('d/m/Y', strtotime('+3 months', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+3 months', strtotime($date)));
 	}
 
 	public static function semestres($date){
-		return date('d/m/Y', strtotime('+6 months', strtotime(self::format($date))));
+		return date('Y/m/d', strtotime('+6 months', strtotime($date)));
 	}
 
 	public static function anos($date){
-		return date('d/m/Y', strtotime('+1 year', strtotime(self::format($date))));
-	}
-
-	private static function format($date){
-		$myDateTime = DateTime::createFromFormat('d-m-Y', $date);
-		return $myDateTime->format('Y-m-d');
+		return date('Y/m/d', strtotime('+1 year', strtotime($date)));
 	}
 	
 }
