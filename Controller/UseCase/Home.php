7c1b91d9baa631af821b947143ad1ccefd66b729
erickstryck <erickstryck@hotmail.com>
2015-10-06 09:30:17 -0300

    situação e prioridade das tarefas são definidas por cores no evento.

--- Controller/UseCase/Home.php
+++ Controller/UseCase/Home.php
@@ -46,26 +46,34 @@ class Home extends GenericController
         Lumine::import("Usuario");
         Lumine::import("UsuarioHasEmpresa");
         Lumine::import("Tarefa");
+        Lumine::import("Prioridade");
+        Lumine::import("Situacao");
         //pegar user da empresa
         $user = new Usuario();
         $has = new UsuarioHasEmpresa();
+        $situacao = new Situacao();
         $user->join($has)->where("empresa_id=" . $_SESSION["empresaId"])->find();
         //pegar tarefas da empresa
         $tar = new Tarefa();
-        $tar->select('titulo as title, data as start')->where("empresa_id=" . $_SESSION['empresaId'] . " and ativo = 1")->find();
-//        $arrayJS = $this->getNiceArray($tar->allToArray());
-//        $arrJS = array('events' => $arrayJS, 'color' => 'red', 'textColor' => 'white');
-//        return json_encode($arrJS);
-        return $tar->allToArray();
+        $prioridade = new Prioridade();
+        $situacao->alias('s');
+        $prioridade->alias('p');
+        $tar->join($prioridade);
+        $tar->join($situacao);
+        $tar->select('titulo as title, data as start, p.des as prioridade,s.des as situacao')->where("empresa_id=" . $_SESSION['empresaId'] . " and ativo = 1 and tarefa_ativa=1")->find();
+        return $this->getNiceArray($tar->allToArray());
     }
 
-    // public function getNiceArray($badArray)
-    // {
-    //     $arrTar = array();
-    //     foreach ($badArray as $value) {
-    //         $temp = array('title' => $value['title'], 'start' => $value['start']);
-    //         array_push($arrTar, $temp);
-    //     }
-    //     return json_encode($arrTar);
-    // }
+    public function getNiceArray($badArray)
+    {
+        $arrTar = array();
+        foreach ($badArray as $value) {
+            if ($value['prioridade'] === "Baixa") $temp = array('title' => $value['title'], 'start' => $value['start'], 'color' => 'green', 'textColor' => 'white');
+            if ($value['prioridade'] === "Normal") $temp = array('title' => $value['title'], 'start' => $value['start'], 'color' => 'yellow', 'textColor' => 'black');
+            if ($value['prioridade'] === "Alta") $temp = array('title' => $value['title'], 'start' => $value['start'], 'color' => 'red', 'textColor' => 'white');
+            if ($value['situacao'] === "Finalizada") $temp = array('title' => $value['title'], 'start' => $value['start'], 'color' => 'black', 'textColor' => 'white');
+            array_push($arrTar, $temp);
+        }
+        return $arrTar;
+    }
 }
